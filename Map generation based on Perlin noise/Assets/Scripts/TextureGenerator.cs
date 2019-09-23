d26a6da7faf4e5a2ddab621f087bc2f38665b2c3
skarving <32582402+skarving@users.noreply.github.com>
2019-09-23 20:15:19 +0200

    Basic noise

--- Map generation based on Perlin noise/Assets/Scripts/TextureGenerator.cs
+++ Map generation based on Perlin noise/Assets/Scripts/TextureGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TextureGenerator : MonoBehaviour
 {
     [Range(2, 512)] public int resolution = 256;
+    public float frequency = 10f;
 
     private Texture2D texture;
 
@@ -24,6 +25,15 @@ public class TextureGenerator : MonoBehaviour
         FillTexture();
     }
 
+    private void Update()
+    {
+        if(transform.hasChanged)
+        {
+            transform.hasChanged = false;
+            FillTexture();
+        }
+    }
+
     public void FillTexture()
     {
         if(texture.width != resolution)
@@ -31,14 +41,23 @@ public class TextureGenerator : MonoBehaviour
             texture.Resize(resolution, resolution);
         }
 
+        Vector3 corner00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
+        Vector3 corner10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+        Vector3 corner01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+        Vector3 corner11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+
         float stepSize = 1f / resolution;
 
-        for(int yIndex = 0; yIndex < resolution; yIndex++)
+        // To compute the colors at the center of the pixels I have to add a half one to achieve this
+        for (int yIndex = 0; yIndex < resolution; yIndex++)
         {
-            for(int xIndex = 0; xIndex < resolution; xIndex++)
+            Vector3 interpolation0 = Vector3.Lerp(corner00, corner01, (yIndex + 0.5f) * stepSize);
+            Vector3 interpolation1 = Vector3.Lerp(corner10, corner11, (yIndex + 0.5f) * stepSize);
+            for (int xIndex = 0; xIndex < resolution; xIndex++)
             {
-                // To compute the colors at the center of the pixels I have to add a half one to achieve this
-                texture.SetPixel(xIndex, yIndex, new Color((xIndex + .5f) * stepSize, (yIndex + .5f) * stepSize, 0f));
+                Vector3 point = Vector3.Lerp(interpolation0, interpolation1, (xIndex + 0.5f) * stepSize);
+
+                texture.SetPixel(xIndex, yIndex, Color.white * Noise.Value(point, frequency));
             }
         }
 
