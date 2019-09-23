bd0578195036b3dcf50b261e31d453f99777363d
skarving <32582402+skarving@users.noreply.github.com>
2019-09-23 14:40:24 +0200

    Texture creation

---
+++ Map generation based on Perlin noise/Assets/Editor/TextureGeneratorEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TextureGenerator))]
+public class TextureGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+
+        if(EditorGUI.EndChangeCheck() && Application.isPlaying)
+        {
+            (target as TextureGenerator).FillTexture();
+        }
+    }
+}
---
+++ Map generation based on Perlin noise/Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureGenerator : MonoBehaviour
+{
+    [Range(2, 512)] public int resolution = 256;
+
+    private Texture2D texture;
+
+    private void OnEnable()
+    {
+        if(texture == null)
+        {
+            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+            texture.name = "Procedural texture";
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Trilinear;
+            texture.anisoLevel = 9;
+
+            GetComponent<MeshRenderer>().material.mainTexture = texture;
+        }
+
+        FillTexture();
+    }
+
+    public void FillTexture()
+    {
+        if(texture.width != resolution)
+        {
+            texture.Resize(resolution, resolution);
+        }
+
+        float stepSize = 1f / resolution;
+
+        for(int yIndex = 0; yIndex < resolution; yIndex++)
+        {
+            for(int xIndex = 0; xIndex < resolution; xIndex++)
+            {
+                // To compute the colors at the center of the pixels I have to add a half one to achieve this
+                texture.SetPixel(xIndex, yIndex, new Color((xIndex + .5f) * stepSize, (yIndex + .5f) * stepSize, 0f));
+            }
+        }
+
+        texture.Apply();
+    }
+}
